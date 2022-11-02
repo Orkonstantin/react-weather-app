d0079b40fbac0cd755db15e3f218d35b2c58ebbc
or konstantin <104775108+Orkonstantin@users.noreply.github.com>
2022-11-02 02:40:14 +0200

    Uploading weather app

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'dc3a0f9ae0mshfe4d4c004bc0c07p1d6721jsn3384577f5bba',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="9909aa97b855189416126bbb123145ab";
